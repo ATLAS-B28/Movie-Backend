00163b0c155cb879afaa723e03e40258b6d789e4
ATLAS-B28 <adityacbhambere28@gmail.com>
2023-10-21 12:55:23 +0530

    Movie app backend with Spring Boot with MongoDB

---
+++ movie/src/main/java/com/example/movie/movies/ReviewService.java
@@ -0,0 +1,30 @@
+package com.example.movie.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Optional<Review> createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("movieId").is(imdbId))
+                .apply(new Update().push("reviews").value(review))
+                .first();
+
+        return Optional.of(review);
+    }
+}
